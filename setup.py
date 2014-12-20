8cc3d386d4da27d4e43efedbfbb1b91c6c8d0d54
Chozabu <chozabu@gmail.com>
2014-12-20 02:15:00 +0000

    update setup.py for p4a

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
- 
+from os import environ, remove
+from os.path import dirname, join, isfile
 from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.extension import Extension
+try:
+    from Cython.Build import cythonize
+    from Cython.Distutils import build_ext
+    have_cython = True
+except ImportError:
+    have_cython = False 
 
 setup(
     ext_modules = cythonize("particles.pyx")
